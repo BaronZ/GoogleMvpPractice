09135eafe7f80d1f8c7b580b98cb964f4dcd3d01
BaronZ <realbo89@outlook.com>
2016-10-10 11:26:58 +0800

    排序的东西放到RankModel

--- app/src/main/java/com/zzb/googlemvppractice/model/live/LiveModel.java
+++ app/src/main/java/com/zzb/googlemvppractice/model/live/LiveModel.java
@@ -2,13 +2,7 @@ package com.zzb.googlemvppractice.model.live;
 
 import com.zzb.googlemvppractice.entity.User;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by ZZB on 2016/10/10.
@@ -16,61 +10,26 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class LiveModel {
 
-    private final Map<Long, Integer> mRankMap = new ConcurrentHashMap<>();
-    private final List<User> mRankUsers = Collections.synchronizedList(new ArrayList<User>());
+    private RankModel mRankModel;
+
+    public LiveModel() {
+        mRankModel = new RankModel();
+    }
 
     public void getLiveShowData() {
 
     }
 
     public void updateRank(User... users) {
-        if (users == null) {
-            return;
-        }
-        List<User> userList = new ArrayList<>();
-        for (User user : users) {
-            userList.add(user);
-        }
-        updateRank(userList);
+        mRankModel.updateRank(users);
     }
 
     public void updateRank(List<User> users) {
-        if (users == null) {
-            return;
-        }
-        Iterator<User> it = users.iterator();
-        while (it.hasNext()) {
-            User user = it.next();
-            if (mRankUsers.contains(user)) {
-                it.remove();
-            }
-        }
-        mRankUsers.addAll(users);
-        sortRank();
+        mRankModel.updateRank(users);
     }
 
-    private void sortRank() {
-        mRankMap.clear();
-        Collections.sort(mRankUsers, new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return o1.getScore() - o2.getScore();
-            }
-        });
-        for (int i = 0; i < mRankUsers.size(); i++) {
-            User user = mRankUsers.get(i);
-            mRankMap.put(user.getUid(), i + 1);
-        }
-
-    }
-
-    public int getRank(long uid) {
-        Integer rank = mRankMap.get(uid);
-        return rank != null ? rank : 0;
-    }
 
     public void leaveLiveRoom() {
-        mRankMap.clear();
-        mRankUsers.clear();
+        mRankModel.clear();
     }
 }
---
+++ app/src/main/java/com/zzb/googlemvppractice/model/live/RankModel.java
@@ -0,0 +1,73 @@
+package com.zzb.googlemvppractice.model.live;
+
+import com.zzb.googlemvppractice.entity.User;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by ZZB on 2016/10/10.
+ */
+
+public class RankModel {
+
+    private final Map<Long, Integer> mRankMap = new ConcurrentHashMap<>();
+    private final List<User> mRankUsers = Collections.synchronizedList(new ArrayList<User>());
+
+
+    public void updateRank(User... users) {
+        if (users == null) {
+            return;
+        }
+        List<User> userList = new ArrayList<>();
+        for (User user : users) {
+            userList.add(user);
+        }
+        updateRank(userList);
+    }
+
+    public void updateRank(List<User> users) {
+        if (users == null) {
+            return;
+        }
+        Iterator<User> it = users.iterator();
+        while (it.hasNext()) {
+            User user = it.next();
+            if (mRankUsers.contains(user)) {
+                it.remove();
+            }
+        }
+        mRankUsers.addAll(users);
+        sortRank();
+    }
+
+    private void sortRank() {
+        mRankMap.clear();
+        Collections.sort(mRankUsers, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getScore() - o2.getScore();
+            }
+        });
+        for (int i = 0; i < mRankUsers.size(); i++) {
+            User user = mRankUsers.get(i);
+            mRankMap.put(user.getUid(), i + 1);
+        }
+
+    }
+
+    public int getRank(long uid) {
+        Integer rank = mRankMap.get(uid);
+        return rank != null ? rank : 0;
+    }
+
+    public void clear() {
+        mRankMap.clear();
+        mRankUsers.clear();
+    }
+}
