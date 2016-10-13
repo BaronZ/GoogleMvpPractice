3d19b2c8740e982adae5e823129aae4931dd7ac9
BaronZ <realbo89@outlook.com>
2016-10-13 15:40:31 +0800

    TestSubscriber#getOnNextEvents 方法试用

--- app/src/test/java/com/zzb/googlemvppractice/model/LoginModelTest.java
+++ app/src/test/java/com/zzb/googlemvppractice/model/LoginModelTest.java
@@ -8,8 +8,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.List;
 
 import rx.observers.TestSubscriber;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Created by ZZB on 2016/10/13.
@@ -17,31 +21,46 @@ import rx.observers.TestSubscriber;
 
 public class LoginModelTest {
     LoginModel mLoginModel;
+    TestSubscriber<User> mUserTestSubscriber;
     @Before
     public void setUp(){
         mLoginModel = new LoginModel(new UserModel());
+        mUserTestSubscriber = new TestSubscriber<>();
     }
 
     @Test
     public void rx_onNext_test(){
-        TestSubscriber<User> testSubscriber = new TestSubscriber<>();
         String phone = "1";
         User expectedUser = new User(phone.hashCode());
         expectedUser.setNick(phone);
 
-        mLoginModel.phoneLogin(phone, "1").subscribe(testSubscriber);
-        testSubscriber.assertNoErrors();
-        testSubscriber.assertReceivedOnNext(Collections.singletonList(expectedUser));
-        testSubscriber.assertCompleted();
+        mLoginModel.phoneLogin(phone, "1").subscribe(mUserTestSubscriber);
+        mUserTestSubscriber.assertNoErrors();
+        mUserTestSubscriber.assertReceivedOnNext(Collections.singletonList(expectedUser));
+        mUserTestSubscriber.assertCompleted();
 
     }
     @Test
-    public void rx_onError_test(){
-        TestSubscriber<User> testSubscriber = new TestSubscriber<>();
+    public void rx_getOnNextEvent_test(){
         String phone = "1";
-        mLoginModel.phoneLogin(phone, "2").subscribe(testSubscriber);
-        testSubscriber.assertError(RuntimeException.class);
-        testSubscriber.assertNotCompleted();
+        User expectedUser = new User(phone.hashCode());
+        expectedUser.setNick(phone);
+
+        mLoginModel.phoneLogin(phone, "1").subscribe(mUserTestSubscriber);
+        mUserTestSubscriber.assertNoErrors();
+        mUserTestSubscriber.assertCompleted();
+//        mUserTestSubscriber.assertReceivedOnNext(Collections.singletonList(expectedUser));
+        List<User> users = mUserTestSubscriber.getOnNextEvents();
+        assertNotNull(users);
+        assertEquals(1, users.size());
+        assertEquals(phone, users.get(0).getNick());
+    }
+    @Test
+    public void rx_onError_test(){
+        String phone = "1";
+        mLoginModel.phoneLogin(phone, "2").subscribe(mUserTestSubscriber);
+        mUserTestSubscriber.assertError(RuntimeException.class);
+        mUserTestSubscriber.assertNotCompleted();
 
     }
 }
